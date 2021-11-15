db69bea4005fea1ef82a6c697ecf6deb4ff058b1
shubham05Mishra <shubham.mishra@winlancers.com>
2021-11-15 18:31:28 +0530

    Forgot Password Page

---
+++ FibesApp/FibesApp/ViewModels/Accounts/ForgotPasswordViewModel.cs
@@ -0,0 +1,78 @@
+﻿using Acr.UserDialogs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace FibesApp.ViewModels.Accounts
+{
+   public class ForgotPasswordViewModel : BaseViewModel
+    {
+        #region Constructor
+        public ForgotPasswordViewModel(INavigation nav)
+        {
+            Navigation = nav;
+            ResetCommand = new Command(OnResetAsync);
+            SignInCommand = new Command(OnSignInAsync);
+        }
+        #endregion
+
+        #region Command
+        public Command SignInCommand { get; }
+        public Command ResetCommand { get; }
+
+        #endregion
+
+        #region Properties
+        private string _Email;
+        public string Email
+        {
+            get { return _Email; }
+            set
+            {
+                if (_Email != value)
+                {
+                    _Email = value;
+                    OnPropertyChanged("Email");
+                }
+            }
+        }
+        #endregion
+
+        #region Method
+        /// <summary>
+        /// TODO:To Call The SignIn  ...
+        /// </summary>
+        private async void OnSignInAsync(object obj)
+        {
+            await Navigation.PushModalAsync(new Views.Accounts.LoginView());
+
+        }
+        /// <summary>
+        /// TODO:To Call The Reset button ...
+        /// </summary>
+        private void OnResetAsync(object obj)
+        {
+            if (!Validate())
+            {
+                return;
+            }
+        }
+
+        /// <summary>
+        /// TODO : To Apply Sign In Validations...
+        /// </summary>
+        private bool Validate()
+        {
+            if (string.IsNullOrEmpty(Email))
+            {
+                UserDialogs.Instance.Alert("Please enter email.");
+                return false;
+            }
+            UserDialogs.Instance.HideLoading();
+            return true;
+
+        }
+        #endregion
+    }
+}
--- FibesApp/FibesApp/Views/Accounts/ForgotPasswordView.xaml.cs
+++ FibesApp/FibesApp/Views/Accounts/ForgotPasswordView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FibesApp.ViewModels.Accounts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,17 @@ namespace FibesApp.Views.Accounts
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ForgotPasswordView : ContentPage
     {
+        //TODO : To Define Local Class Level Variables...
+        protected ForgotPasswordViewModel ForgotPasswordVM;
+        //TODO : To Define cosntructor...
+        #region Constructor
         public ForgotPasswordView()
         {
             InitializeComponent();
+            ForgotPasswordVM = new ForgotPasswordViewModel(this.Navigation);
+            this.BindingContext = ForgotPasswordVM;
+
         }
+        #endregion
     }
 }
