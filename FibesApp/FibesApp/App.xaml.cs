bdf6a6a94fa904ffcb0aa5370169741a6827b6cb
Shubham05Mishra <shubham.mishra@winlancers.com>
2021-11-15 13:26:37 +0530

    Add Static Colours in App.xaml

--- FibesApp/FibesApp/App.xaml.cs
+++ FibesApp/FibesApp/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using FibesApp.Views.Accounts;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,7 @@ namespace FibesApp
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new LoginView();
         }
 
         protected override void OnStart()
