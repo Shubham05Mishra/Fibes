61acb0464b8346171b79583c41df5a0074f2926d
shubham05Mishra <shubham.mishra@winlancers.com>
2021-11-17 11:09:08 +0530

    BoxDetailView

---
+++ FibesApp/FibesApp/Views/Box/BoxDetailView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FibesApp.Views.Box
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BoxDetailView : ContentPage
+    {
+        public BoxDetailView()
+        {
+            InitializeComponent();
+        }
+    }
+}
