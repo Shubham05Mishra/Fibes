dc02490647514eda7a198718d3f546d200a2dbf1
Shubham05Mishra <shubham.mishra@winlancers.com>
2021-11-17 11:59:19 +0530

    EmptyBoxViewModel

--- FibesApp/FibesApp/ViewModels/Box/EmptyBoxViewModel.cs
+++ FibesApp/FibesApp/ViewModels/Box/EmptyBoxViewModel.cs
@@ -1,10 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Xamarin.Forms;
 
 namespace FibesApp.ViewModels.Box
 {
     public class EmptyBoxViewModel : BaseViewModel
     {
+        #region Constructor
+        public EmptyBoxViewModel(INavigation nav)
+        {
+            Navigation = nav;
+            BackToHomeCommand = new Command(BackToHomeAsync);
+        }
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Commands
+        public Command BackToHomeCommand { get; }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// TODO:To Call The BackToHome button ...
+        /// </summary>
+        private void BackToHomeAsync(object obj)
+        {
+            App.Current.MainPage = new Views.Home.HomeView();
+        }
+        #endregion
     }
 }
